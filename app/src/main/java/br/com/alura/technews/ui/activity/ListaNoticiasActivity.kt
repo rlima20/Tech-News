b76d0d0173be264eeb10e0341bb013abf03f83ef
Raphael Lima <raphael.lima-ext@ab-inbev.com>
2021-05-09 00:21:14 -0300

    [feature/master/fragments] - Migration from Activities to Fragments

--- app/src/main/java/br/com/alura/technews/ui/activity/ListaNoticiasActivity.kt
+++ app/src/main/java/br/com/alura/technews/ui/activity/ListaNoticiasActivity.kt
@@ -19,64 +19,13 @@ import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 
 private const val TITULO_APPBAR = "Notícias"
-private const val MENSAGEM_FALHA_CARREGAR_NOTICIAS = "Não foi possível carregar as novas notícias"
 
 class ListaNoticiasActivity : AppCompatActivity() {
-
-    /**
-     * Com essa abordagem de injetar o repository eu não preciso mais fazer a instância do
-     * banco de dados, que já está sendo injetada dentro do NoticiaRepository que é injetado pelo koin.
-     *
-     * Ele vai injetar o NoticiaRepository e dentro dele, ele já sabe que precisa do dao. E internamente
-     * o koin já sabe como criar o dao.
-     *
-     * O mesmo acontece com o viewmodel. O koin já sabe como fazer a injeção de dependência. Logo eu não prciso mais
-     * dessa linha de codigo:     private val repository: NoticiaRepository by inject()
-     *
-     */
-
-    private val adapter by lazy {
-        ListaNoticiasAdapter(context = this)
-    }
-
-    private val viewModel:ListaNoticiasViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_noticias)
         title = TITULO_APPBAR
-        configuraRecyclerView()
-        configuraFabAdicionaNoticia()
-        buscaNoticias()
-    }
-
-    private fun configuraFabAdicionaNoticia() {
-        activity_lista_noticias_fab_salva_noticia.setOnClickListener {
-            abreFormularioModoCriacao()
-        }
-    }
-
-    private fun configuraRecyclerView() {
-        val divisor = DividerItemDecoration(this, VERTICAL)
-        activity_lista_noticias_recyclerview.addItemDecoration(divisor)
-        activity_lista_noticias_recyclerview.adapter = adapter
-        configuraAdapter()
-    }
-
-    private fun configuraAdapter() {
-        adapter.quandoItemClicado = this::abreVisualizadorNoticia
-    }
-
-    private fun buscaNoticias() {
-        viewModel.buscaTodos().observe(this, Observer { resource ->
-            resource.dado?.let {
-                adapter.atualiza(it)
-            }
-
-            resource.erro?.let {
-                mostraErro(MENSAGEM_FALHA_CARREGAR_NOTICIAS)
-            }
-        })
     }
 
     private fun abreFormularioModoCriacao() {
@@ -89,13 +38,5 @@ class ListaNoticiasActivity : AppCompatActivity() {
         intent.putExtra(NOTICIA_ID_CHAVE, it.id)
         startActivity(intent)
     }
-
-    /**
-     * Commit raphael - primeira alteração
-     * Commit2
-     * Commit3
-     * Commit3
-     * Commit4
-     */
 
 }
---
+++ app/src/main/java/br/com/alura/technews/ui/fragment/ListaNoticiasFragment.kt
@@ -0,0 +1,65 @@
+package br.com.alura.technews.ui.fragment
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import br.com.alura.technews.ui.fragment.extensions.mostraErro
+import br.com.alura.technews.ui.recyclerview.adapter.ListaNoticiasAdapter
+import br.com.alura.technews.ui.viewmodel.ListaNoticiasViewModel
+import kotlinx.android.synthetic.main.lista_noticia.*
+import org.koin.android.viewmodel.ext.android.viewModel
+
+private const val MENSAGEM_FALHA_CARREGAR_NOTICIAS = "Não foi possível carregar as novas notícias"
+
+class ListaNoticiasFragment : Fragment() {
+    /**
+     * Com essa abordagem de injetar o repository eu não preciso mais fazer a instância do
+     * banco de dados, que já está sendo injetada dentro do NoticiaRepository que é injetado pelo koin.
+     *
+     * Ele vai injetar o NoticiaRepository e dentro dele, ele já sabe que precisa do dao. E internamente
+     * o koin já sabe como criar o dao.
+     *
+     * O mesmo acontece com o viewmodel. O koin já sabe como fazer a injeção de dependência. Logo eu não prciso mais
+     * dessa linha de codigo:     private val repository: NoticiaRepository by inject()
+     *
+     */
+
+    private val adapter by lazy {
+        context?.let {
+            ListaNoticiasAdapter(context = it)
+        } ?: throw IllegalArgumentException("COntexto Inválido")
+    }
+
+    private val viewModel: ListaNoticiasViewModel by viewModel()
+
+
+    private fun configuraFabAdicionaNoticia() {
+        lista_noticias_fab_salva_noticia.setOnClickListener {
+            //abreFormularioModoCriacao()
+        }
+    }
+
+    private fun configuraRecyclerView() {
+        val divisor = DividerItemDecoration(context, LinearLayoutManager.VERTICAL)
+        lista_noticias_recyclerview.addItemDecoration(divisor)
+        lista_noticias_recyclerview.adapter = adapter
+        configuraAdapter()
+    }
+
+    private fun configuraAdapter() {
+        //adapter.quandoItemClicado = this::abreVisualizadorNoticia
+    }
+
+    private fun buscaNoticias() {
+        viewModel.buscaTodos().observe(this, Observer { resource ->
+            resource.dado?.let {
+                adapter.atualiza(it)
+            }
+
+            resource.erro?.let {
+                mostraErro(MENSAGEM_FALHA_CARREGAR_NOTICIAS)
+            }
+        })
+    }
+}
